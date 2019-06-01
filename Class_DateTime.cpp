6361ac8ddde43cd7684fe11889fa5a5c43fcf8c0
alenaaleyn1kova <48382053+alenaaleyn1kova@users.noreply.github.com>
2019-06-01 11:04:35 +0500

    Add files via upload

---
+++ Class_DateTime.cpp
@@ -0,0 +1,12 @@
+﻿#include "pch.h"
+#include <iostream>
+#include <ctime>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	time_t seconds = time(NULL); // число секунд, прошедших с 01.01.1970 - можно вообще обойтись без класса DateTime
+    cout << "Количество прошедших секунд с 01.01.1970: " << seconds << endl; 
+}
